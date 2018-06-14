ad1b9b9cee10127adacc10d7d7c5fa24fc959295
honmanyau <honmanyau@users.noreply.github.com>
2018-06-15 03:09:32 +1000

    Added templates.

---
+++ src/template/scripts.js
@@ -0,0 +1,9 @@
+let message = `It looks like you forgot to edited the HTML template! (◕︿◕✿)`;
+
+console.info('scripts.js successfully loaded! (◕ᴗ◕✿)');
+console.warn(message);
+alert(message);
+
+// ============
+// === Main ===
+// ============
