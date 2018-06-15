ddc0276d0221aa84422243585955cd5d70a15f6b
honmanyau <honmanyau@users.noreply.github.com>
2018-06-15 16:43:08 +1000

    Rewrapped warnining message in if block.

--- src/templates/scripts.js
+++ src/templates/scripts.js
@@ -1,8 +1,11 @@
-let message = `It looks like you forgot to edited the HTML template! (◕︿◕✿)`;
-
 console.info('scripts.js successfully loaded! (◕ᴗ◕✿)');
-console.warn(message);
-alert(message);
+
+if (!document.title) {
+  let message = `It looks like you forgot to edited the HTML template! (◕︿◕✿)`;
+  
+  console.warn(message);
+  alert(message);
+}
 
 // ============
 // === Main ===
