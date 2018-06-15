1abd65c2d1083d988da61c4888c596c54912ceef
honmanyau <honmanyau@users.noreply.github.com>
2018-06-16 03:30:07 +1000

    Cleaned up console.log\'s.

--- src/puzzles/001-blocked-blog/scripts/analytics.js
+++ src/puzzles/001-blocked-blog/scripts/analytics.js
@@ -1,43 +1,34 @@
+"use strict";
 function checkId(id) {
-  return id === '1024some2048api4096key';
+    return id === '1024some2048api4096key';
 }
-
 function initialiseAnalytics(id, callback) {
-  let idIsValid = checkId(id);
-
-  if (idIsValid) {
-    // Simulate delays
-    (function downloadDelay() {
-      let init = window.performance.now();
-
-      while (window.performance.now() - init < 5000) {
-        // Do nothing and block the thread for 5 seconds.
-      }
-    })();
-
-    (function executeDelay() {
-      let init = window.performance.now();
-
-      while (window.performance.now() - init < 1000) {
-        // Do nothing and block the thread for 1 seconds.
-      }
-    })();
-
-    let analyticsApi = {
-      init: function init() {
-        // Does things!
-      }
-    };
-
-    callback(analyticsApi);
-  }
-  else {
-    throw 'TypeError: NetworkError when attempting to fetch resource.';
-  }
+    var idIsValid = checkId(id);
+    if (idIsValid) {
+        // Simulate delays
+        (function downloadDelay() {
+            var init = window.performance.now();
+            while (window.performance.now() - init < 5000) {
+                // Do nothing and block the thread for 5 seconds.
+            }
+        })();
+        (function executeDelay() {
+            var init = window.performance.now();
+            while (window.performance.now() - init < 1000) {
+                // Do nothing and block the thread for 1 seconds.
+            }
+        })();
+        var analyticsApi = {
+            init: function init() {
+                // Does things!
+            }
+        };
+        callback(analyticsApi);
+    }
+    else {
+        throw 'TypeError: NetworkError when attempting to fetch resource.';
+    }
 }
-
-console.log(document.body);
-initialiseAnalytics('1024some2048api4096key', function(analyticsApi) {
-  analyticsApi.init();
+initialiseAnalytics('1024some2048api4096key', function (analyticsApi) {
+    analyticsApi.init();
 });
-console.log(document.body);
