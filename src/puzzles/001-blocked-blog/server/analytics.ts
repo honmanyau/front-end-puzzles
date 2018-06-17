f02111df3eee0ab8a6a1e14f7bd1fda69a809d8b
honmanyau <honmanyau@users.noreply.github.com>
2018-06-17 15:31:16 +1000

    Changed simulated script running time.

--- src/puzzles/001-blocked-blog/server/analytics.ts
+++ src/puzzles/001-blocked-blog/server/analytics.ts
@@ -1,7 +1,7 @@
 (function() {
   let init = window.performance.now();
 
-  while (window.performance.now() - init < 1500) {
+  while (window.performance.now() - init < 1000) {
     // Block the thread for way too long because this is a terrible analytics
     // company that is only interested in money and is unable to hire
     // decent developers
