4ff1de7f75e0d95506547ca238f2d61e17090dfd
honmanyau <honmanyau@users.noreply.github.com>
2018-06-17 13:36:31 +1000

    Added route handling for resources.

--- src/puzzles/server/server.ts
+++ src/puzzles/server/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 
-// Setup
 const app = express();
-
-// Config
 const port = process.env.PORT || 3001;
+
+app.get('/puzzle/:id/:resource', function(request, response) {
+  let { id, resource } = request.params;
+  let parentDir = __dirname.replace(/server$/, '');
+  let path = `${parentDir}/${id}/server/${resource}`;
+
+  response.sendFile(path);
+});
 
 // Start server
 app.listen(port, function() {
