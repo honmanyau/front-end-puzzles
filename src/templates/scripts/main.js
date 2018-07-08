044b06851db92b20438ae5665042cfa129447d51
honmanyau <honmanyau@users.noreply.github.com>
2018-07-09 00:16:10 +1000

    Fixed typo.

--- src/templates/scripts/main.js
+++ src/templates/scripts/main.js
@@ -1,7 +1,7 @@
 console.info('scripts.js successfully loaded! (◕ᴗ◕✿)');
 
 if (!document.title) {
-  let message = `It looks like you forgot to edited the HTML template! (◕︿◕✿)`;
+  let message = `It looks like you forgot to edit the HTML template! (◕︿◕✿)`;
 
   console.warn(message);
   alert(message);
